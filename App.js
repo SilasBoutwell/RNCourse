645923e9e521680b50c9369d3991e36aa1337209
Silas Boutwell <silaslee.b@gmail.com>
2024-09-16 21:08:38 -0700

    added a delete function

--- App.js
+++ App.js
@@ -8,11 +8,13 @@ export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   function addGoalHandler(enteredGoalText) {
-    setCourseGoals(currentCourseGoals => [...currentCourseGoals, { text: enteredGoalText + " ", key: Math.random().toString() },]);
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, { text: enteredGoalText + " ", id: Math.random().toString() },]);
   }
 
-  function deleteGoalHandler() {
-    console.log("DELETE")
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
@@ -23,6 +25,7 @@ export default function App() {
           return (
             <GoalItem
               text={itemData.item.text}
+              id={itemData.item.id}
               onDeleteItem={deleteGoalHandler}
             />
           )
